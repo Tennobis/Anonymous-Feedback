c610752dc15532d41100266cf70ba0bac0daa171
Tennobis <tanveer2004hossain@gmail.com>
2025-07-13 21:44:50 +0530

    changes in sign in schema and sign in page

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import { signInSchema } from "@/schemas/signInSchema";
+import { zodResolver } from "@hookform/resolvers/zod";
+import axios from "axios";
+import { useRouter } from "next/router";
+import React from "react";
+import { useForm } from "react-hook-form";
+import z from "zod";
 
 const page = () => {
-  return (
-    <div>Sign In</div>
-  )
+  const form = useForm<z.infer<typeof signInSchema>>({
+    resolver: zodResolver(signInSchema),
+    defaultValues: {
+      username: "",
+      password: "",
+    },
+  });
+  const router = useRouter();
+const onSubmit = async () =>{
+  await axios.post(`/api/sign-in`)
 }
+  return <div>Sign In</div>;
+};
 
-export default page
+export default page;
--- src/schemas/signInSchema.ts
+++ src/schemas/signInSchema.ts
@@ -1,6 +1,6 @@
 import {z} from 'zod';
 
 export const signInSchema = z.object({
-    email:z.string(),
+    username:z.string(),
     password:z.string()
 })
